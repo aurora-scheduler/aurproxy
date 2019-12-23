cd60c69f29c65628135269d0661d0fe0027cd3fc
thinker0 <thinker0@gmail.com>
2019-12-23 13:51:21 +0900

    Add guppy

--- tellapart/aurproxy/command.py
+++ tellapart/aurproxy/command.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from guppy.heapy import RM
 import gevent.monkey
 gevent.monkey.patch_all()
 
